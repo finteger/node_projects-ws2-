f564ea1e47d908f65a5ed8b6b4e530f55ef144aa
finteger <nashassociatesinc@gmail.com>
2025-11-19 18:36:51 -0700

    Update

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 8080;
+const mainRoutes = require('./routes/mainRoutes');
 
 
-app.listen(port, ()=>{
+app.use(mainRoutes);
+
+
+
+app.listen(port, ()=>{``
     console.log(`Server is running on port ${port}`);
 });
---
+++ routes/mainRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/search', (req, res) =>{
+    const query = req.query.q;
+    res.send(`Query term received: ${query}`);
+});
+
+
+router.get('/', (req, res) =>{
+    res.send('Hello World!');
+});
+
+
+module.exports = router;
