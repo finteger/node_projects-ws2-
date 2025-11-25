11d874f75d03e306349b2037d5d16c8640a83304
finteger <nashassociatesinc@gmail.com>
2025-11-24 17:49:05 -0700

    Update

--- app.js
+++ app.js
@@ -3,9 +3,23 @@ const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 8080;
 const mainRoutes = require('./routes/mainRoutes');
+const userRoutes = require('./routes/users');
+const ejs = require('ejs');
+
+//set view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+//Static files
+app.use(express.static('public'));
+
+//parse incoming request bodies
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 //is a middleware
 app.use(mainRoutes);
+app.use(userRoutes);
 
 
 app.listen(port, ()=>{``
