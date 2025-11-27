55bd418adfbc6ded2d08cefddf61f15709b728b8
finteger <nashassociatesinc@gmail.com>
2025-11-26 17:44:15 -0700

    Update

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 8080;
 const mainRoutes = require('./routes/mainRoutes');
 const userRoutes = require('./routes/users');
 const ejs = require('ejs');
+const rateLimit = require('express-rate-limit');
 
 //set view engine
 app.set('view engine', 'ejs');
@@ -17,7 +18,17 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
+
+//rate limiter
+const limiter = rateLimit({
+    windowsMS: 1 * 15 * 1000, 
+    max: 11,
+    message: 'Too many requests, please try again later.'
+});
+
+
 //is a middleware
+app.use(limiter);
 app.use(mainRoutes);
 app.use(userRoutes);
 
